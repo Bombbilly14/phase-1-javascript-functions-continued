d950619ef2ce2163d17ff5b049df81d60e755ab3
William German <william.j.german2@gmail.com>
2022-11-05 16:44:59 -0700

    commit tree 265fbbd6ac3352a6a4e78e60b5119f02c809f70d

--- index.js
+++ index.js
@@ -1,1 +1,50 @@
 // code your solution here
+function saturdayFun (thing=`roller-skate`) {
+    return `This Saturday, I want to ${thing}!`
+}
+
+function mondayWork (thing=`go to the office`) {
+    return `This Monday, I will ${thing}.`
+
+}
+
+function wrapAdjective (thing=`*`) {
+    return function (thing2=`special`) {
+        return `You are ${thing}${thing2}${thing}!`
+    
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function saturdayFun (activity=`roller-skate`) {
+//     return `This Saturday, I want to ${activity}!`
+
+// }
+// function mondayWork(activity= `go to the office`) {
+//     return `This Monday, I will ${activity}.`
+// }
+// function wrapAdjective (flare=`*`) {
+//     return function(word=`special`) {
+//         return `You are ${flare+word+flare}!`
+
+//     }
+
+// }
